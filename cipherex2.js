bcf6384fa4d59db330afe6debe8334740fec6c60
Aaron Dawson <adawson001@gmail.com>
2025-11-23 17:29:48 -0500

    completed assignment

--- cipherex2.js
+++ cipherex2.js
@@ -1,24 +1,25 @@
-const friend = 'BRUTUS'
-const shiftValue = 3
+const friend = 'BRUTUS';
+const shiftValue = 3;
 //Step1
-let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 //Step2
-const firstLetter = friend[0]
-friend.replace('B', 'b')
-const index = alphabet.indexOf(firstLetter.toLowerCase())
+const firstLetter = friend[0];
+const index = alphabet.indexOf(firstLetter.toLowerCase());
 //Q1
 //The index is 1 instead of 2 because arrays are indexed starting at "0"; 0 = A, 1 = B
 //Step3
-const newIndex = firstLetter + shiftValue;
+const newIndex = index + shiftValue;
 const encryptedFirstLetter = alphabet[newIndex];
 //Q2
 //the modulus operator (%)
 //Step4
-const alphabetLength = alphabet.length;
+//const alphabetLength = alphabet.length;
 //Step5
-
+const alphabetLength = alphabet.length;
+const newIndex2 = (index + shiftValue) % alphabetLength;
+const encryptedSecondLetter = alphabet[newIndex2];
 //Step6
-friend.slice(0, 3)
+friend.slice(0, 3);
 
 
 
