d8ee95c12b080db7cd5305ca3c8c5f0d97eb98ea
Aaron Dawson <adawson001@gmail.com>
2025-11-23 15:29:59 -0500

    added step 6, added comments

--- cipherex2.js
+++ cipherex2.js
@@ -9,13 +9,16 @@ const index = alphabet.indexOf(firstLetter.toLowerCase())
 //Q1
 //The index is 1 instead of 2 because arrays are indexed starting at "0"; 0 = A, 1 = B
 //Step3
-const newIndex = index + 3;
+const newIndex = firstLetter + shiftValue;
 const encryptedFirstLetter = alphabet[newIndex];
 //Q2
 //the modulus operator (%)
 //Step4
 const alphabetLength = alphabet.length;
 //Step5
+
+//Step6
+friend.slice(0, 3)
 
 
 
