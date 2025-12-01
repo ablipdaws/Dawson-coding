fc3349dcbc5cf41339c684498d177e9e6dd21c05
Aaron Dawson <adawson001@gmail.com>
2025-12-01 16:30:01 -0500

    FizzBuzz attempt
    	

---
+++ FizzBuzz5.8.23.js
@@ -0,0 +1,49 @@
+//let n < 100
+//let arr = [1,2,3]
+//console.log(arr)
+//function divide (i / 3);
+let i = 1
+let n = 100
+for (let i = 1; i < n; i++);
+{
+    const remainderBy3 = i % 3; //print 'Fizz'
+    const remainderBy5 = i % 5; //print 'Buzz'
+    if (remainderBy3 === 0 && remainderBy5 === 0);{
+        console.log('FizzBuzz');
+    }
+    else if (remainderBy3 === 0);{
+        console.log('Fizz');
+    }
+    else if (remainderBy5 === 0);{
+        console.log('Buzz');
+    }
+    else if (remainderBy3 !== 0 || remainderBy5 !== 0){
+        console.log(i);
+    }
+ }
+
+
+
+
+
+
+//function divide(i , 3); fx only declared at top level inside block    
+//while (i / 3 = 'even') {
+//    console.log('Fizz');
+//}
+//while (i / 5 = 'even'){
+//    console.log("Buzz");
+//    {
+//    console.log (i);
+//}
+
+//let i = 1
+//for (let i = 1; i < 100; i++);{
+//  console.log(i);
+//}
+//for (let i = 1; i <=10; i++) {
+//    console.log(i)
+//}
+//vs was not letting me save previous lines without declaring "let i = 0" copied and pasted example
+//from springboard for comparison.
+
