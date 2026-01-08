731a2777a4c17638d090a182ab6353a4921aa3cf
Aaron Dawson <adawson001@gmail.com>
2026-01-08 15:21:10 -0500

    edited submission

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -5,15 +5,34 @@
 //if operation fails you get 1 days of vacation
 //you always get 3 days of vacation for attendance
 //this year you will attend 20 mystery operations
-    try {
-        function mysteryOperation(input) {
-            console.log("Operation successful: " + input);
-            return input; //was missing this line
-        }
-    } catch (error) {
-        console.log(error.message);
-        return "Operation failed";
-    }
 
 
-//function mysteryOperation(_input) vs (input)
+// Mystery operation function
+function mysteryOperation() {
+  // Randomly succeed or fail
+  if (Math.random() < 0.5) {
+    console.log("Mission successful");
+  } else {
+    throw new Error("Mission failed");
+  }
+}
+
+let totalVacationDays = 0;
+const missions = 20;
+
+for (let i = 1; i <= missions; i++) {
+  try {
+    mysteryOperation();
+    // Success reward
+    totalVacationDays += 13;
+  } catch (error) {
+    console.log(error.message);
+    // Failure reward
+    totalVacationDays += 1;
+  } finally {
+    // Attendance reward (always)
+    totalVacationDays += 3;
+  }
+}
+
+console.log(`You deserve ${totalVacationDays} days of vacation!`);
