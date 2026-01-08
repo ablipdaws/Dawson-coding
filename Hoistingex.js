f0e015f1da1729364d2209de16070bfb5e2af739
Aaron Dawson <adawson001@gmail.com>
2026-01-07 19:51:04 -0500

    TimeTravel Exercise

---
+++ Hoistingex.js
@@ -0,0 +1,18 @@
+let destination = "Ancient Egypt";
+    console.log(destination);
+
+destination = "Medeival Europe"
+    console.log(destination);
+
+const travelDate = "2024-03-15";
+//travelDate = "2024-04-26"
+    console.log(travelDate);
+//the code window in Chrome shows the redeclared variable with a red underline and "X"
+//the console shows error "assignment to constant variable"
+
+newVariable = "timeMachineModel";
+var timeMachineModel = "T-800"; 
+    console.log(timeMachineModel);
+//when declaring newVariable, console prints timeMachineModel
+//after using var, console prints T-800    
+
