caeb5013d94f6b56ce102e1ecef183392228fd18
Aaron Dawson <adawson001@gmail.com>
2026-02-01 20:31:54 -0500

    Capstone Ideas

---
+++ CapstoneIdeas.js
@@ -0,0 +1,11 @@
+/*Capstone Ideas
+
+1. Plant identification app--This would be a simple app that would store information an pictures of various plants.  Features would include the ability to search certain regions and see what plants grow there.  You would also be able to take a picture of a plant and submit it to the site and have the site identify it for you.
+
+2. Movie database app--This app would store information about movies.  It would have a social media aspect--mainly the ability to comment on movies that people
+in your friends list have seen.  There would also be a rating aspect and would contain aggregate rating data from sites like IMdB and Rotten Tomatoes.
+
+
+3. Book database app--I often find that I have read a book or an article someplace that is relevant to something I am discussing or am working on.  
+This would be a database that would have information on books and the ability to store 
+books, magazines, and articles in one place for easy user retrieval.*/
